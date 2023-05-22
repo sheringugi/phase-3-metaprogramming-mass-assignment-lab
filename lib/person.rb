90f9d94d63e5157392ba48f0403db1e1faedda4f
sheringugi <sheri.ngugi17@gmail.com>
2023-05-22 11:25:13 +0300

    complete assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
-  # your code here
+ def initialize(attrs) 
+    attrs.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+ end
 end
+attrs= {
+  name: "Jane", 
+  birthday:19, 
+  hair_color:"brown",
+  eye_color:"amber", 
+  height: 5,
+  weight: 56, 
+  handed:"right", 
+  complexion:"light", 
+  t_shirt_size:"Medium",
+  wrist_size:1, 
+  glove_size:"small", 
+  pant_length:32, 
+  pant_width:28
+}
